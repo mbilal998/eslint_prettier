71fc8b88e54c3eeb2f2519c1601d5397e7ef9398
MuhammadBilal <m.bilal998@gmail.com>
2021-01-20 11:09:20 +0500

    Working on eslint rules

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const x = 1;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
